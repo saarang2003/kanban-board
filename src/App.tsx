d5d5de67b84f170a481937ccfb84fe6d538f896d
sarang <saranggp2018@gmail.com>
2026-02-19 11:55:58 +0530

    first commit-> project setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@mui/material";
+import "./App.css";
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Button variant="contained">Add</Button>
+    </>
+  );
+}
+
+export default App;
